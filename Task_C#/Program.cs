49fb70ce3c92584889fcb5a262313d47dc37ea67
unknown <ot250013@yandex.ru>
2022-10-01 00:24:18 +0300

    add C# programm

--- Task_C#/Program.cs
+++ Task_C#/Program.cs
@@ -11,3 +11,35 @@
 [“Russia”, “Denmark”, “Kazan”] → []
 */
 
+
+string[] arrayString = { "Hello", "world", "GB", "HTML", "123", "VSC", "Print", "WEB", "Kotik", "WWW", "Geek" };
+string[] newArray = new string[arrayString.Length];
+
+
+string[] СreatingArrayNoMoreThreeChar(string[] oldArray, string[] newArray)  // метод создания массива с элементами не более 3х символов
+{
+    int count = 0;
+    for (int i = 0; i < oldArray.Length; i++)                             // проходим циклом по массиву
+
+        if (oldArray[i].Length <= 3)
+        {
+            newArray[count] = oldArray[i];                                // создаем новый массив по индексу с длиной <= 3 символа
+            count++;
+        }
+    return newArray;                                                       // возврат, т.к. метод не VOID 
+}
+
+
+
+void PrintArray(string[] array)                                            // метод печати в консоль
+{
+    Console.Write(" Элементы массива в которых менее трех символов: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+СreatingArrayNoMoreThreeChar(arrayString, newArray);                       // вызов метода + передача двух аргументов
+PrintArray(newArray);
+
